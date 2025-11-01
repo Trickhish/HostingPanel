610f51fb184ae4c447d941fb452289ac04d0512e
Charles-Ivan Dury <charlesivan.dury@gmx.fr>
2025-11-01 15:36:08 +0100

    ui update

--- client/src/app/pages/login/login.component.ts
+++ client/src/app/pages/login/login.component.ts
@@ -7,5 +7,40 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+    resetErr() {
+      document.getElementById("err")!.innerHTML = "";
+      document.getElementById("err")!.classList.remove("active");
+  }
 
+  err(msg:String, type="info") {
+      var icon="fa-solid fa-exclamation";
+
+      switch (type) {
+          case "error":
+              icon="error fa-solid fa-circle-exclamation";
+              break;
+          case "warning":
+              icon="warning fa-solid fa-triangle-exclamation";
+              break;
+          default:
+              icon="info fa-solid fa-exclamation"
+      }
+
+      document.getElementById("err")!.innerHTML = '<i class="'+icon+'"></i> '+msg;
+      document.getElementById("err")!.classList.add("active");
+  }
+
+  showPass() {
+      if (document.getElementById("pass")!.getAttribute("type")=="password") {
+          document.getElementById("pass")!.setAttribute("type", "text");
+
+          document.getElementById("pass_eye")!.classList.add("fa-eye-slash");
+          document.getElementById("pass_eye")!.classList.remove("fa-eye");
+      } else {
+          document.getElementById("pass")!.setAttribute("type", "password");
+
+          document.getElementById("pass_eye")!.classList.remove("fa-eye-slash");
+          document.getElementById("pass_eye")!.classList.add("fa-eye");
+      }
+  }
 }
